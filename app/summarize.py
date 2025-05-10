57aa4a4b1b9294b47bc4984844ebf24de22395bb
stchalon <65845891+stchalon@users.noreply.github.com>
2025-05-10 20:47:37 +0200

    Create summarize.py

---
+++ app/summarize.py
@@ -0,0 +1,14 @@
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+import sys
+import os
+
+def generate_exec_summary(text, sentence_count=3):
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, sentence_count)
+
+    summary_text = "\n".join(str(sentence) for sentence in summary)
+    return summary_text
+  
