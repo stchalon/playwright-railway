0838b1f73384c66bfbc12c9ace4529b3fccb9eff
stchalon <65845891+stchalon@users.noreply.github.com>
2025-05-10 21:57:02 +0200

    Update summarize.py

--- app/summarize.py
+++ app/summarize.py
@@ -1,17 +1,15 @@
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
-import sys
-import os
 import nltk
 
-# Ensure the punkt tokenizer is available
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
-
 def generate_exec_summary(text, sentence_count=3):
+    # Ensure punkt is available at runtime
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+
     parser = PlaintextParser.from_string(text, Tokenizer("english"))
     summarizer = LsaSummarizer()
     summary = summarizer(parser.document, sentence_count)
